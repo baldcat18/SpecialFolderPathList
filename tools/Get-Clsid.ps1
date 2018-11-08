01010f617b41d1761cf41e6edb460e0d6f24eb61
baldcat18 <baldcat@outlook.jp>
2018-11-08 20:46:47 +0900

    CLSIDを大文字に統一

--- tools/Get-Clsid.ps1
+++ tools/Get-Clsid.ps1
@@ -21,7 +21,7 @@ reg.exe export HKCR\CLSID $regFile > $null
 
 Get-Content -LiteralPath $regFile |
 	Where-Object { $_ -match "^\[HKEY_CLASSES_ROOT\\CLSID\\({.+?})\\(?:ShellFolder|shell(?:ex)?)\]$"} |
-	ForEach-Object { $matches[1] } |
+	ForEach-Object { $matches[1].ToUpper() } |
 	Get-Unique |
 	Where-Object { $All -or !$datatext.Contains($_) } |
 	ForEach-Object {
