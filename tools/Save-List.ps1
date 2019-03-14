5b858957dd34d9d2f60d93db45bbd105dcdc7d47
baldcat18 <baldcat@outlook.jp>
2019-03-14 21:57:40 +0900

    Save-List.ps1でファイルを作成した時に、前回作成したファイルと比較するようにした

--- tools/Save-List.ps1
+++ tools/Save-List.ps1
@@ -14,5 +14,13 @@ $edition = (Get-Item "HKLM:/SOFTWARE/Microsoft/Windows NT/CurrentVersion").GetVa
 $now = Get-Date -Format yyyyMMdd-HHmmss
 
 $workDir = (Resolve-Path "$($MyInvocation.MyCommand.Path)\..\..").Path
+
 cscript.exe //u //nologo "$workDir\src\misc\wsh\SpecialFolderPathList.wsf" /bom /dbg /dir /c /d /f /t |
 	Out-File "$workDir\tools\$osVersion $cpu $edition $now.txt" -Encoding UTF8
+
+Push-Location $workDir\tools
+
+$txtFiles = Get-ChildItem "$osVersion $cpu $edition *.txt" | Sort-Object -Property LastWriteTime -Descending
+if (@($txtFiles).Length -ge 2) { fc.exe /n /20 $txtFiles[1].Name $txtFiles[0].Name }
+
+Pop-Location
