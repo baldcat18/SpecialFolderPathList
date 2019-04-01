32b86ece99907ea534e704c5172ff9041061424a
baldcat18 <baldcat@outlook.jp>
2019-04-01 21:39:27 +0900

    tools/Save-List.ps1とSet-Index.ps1をPowerShell Coreに対応させた

--- tools/Save-List.ps1
+++ tools/Save-List.ps1
@@ -10,13 +10,16 @@ Set-StrictMode -Version Latest
 
 $osVersion = [System.Environment]::OSVersion.Version.ToString(3)
 $cpu = $env:PROCESSOR_ARCHITECTURE
-$edition = (Get-Item "HKLM:/SOFTWARE/Microsoft/Windows NT/CurrentVersion").GetValue("EditionID")
+$edition = (Get-Item 'HKLM:/SOFTWARE/Microsoft/Windows NT/CurrentVersion').GetValue('EditionID')
 $now = Get-Date -Format yyyyMMdd-HHmmss
 
+$tempFile = [System.IO.Path]::GetTempFileName()
 $workDir = (Resolve-Path "$($MyInvocation.MyCommand.Path)\..\..").Path
 
-cscript.exe //u //nologo "$workDir\src\misc\wsh\SpecialFolderPathList.wsf" /bom /dbg /dir /c /d /f /t |
-	Out-File "$workDir\tools\$osVersion $cpu $edition $now.txt" -Encoding UTF8
+cmd.exe /c cscript.exe //u //nologo "$workDir\src\misc\wsh\SpecialFolderPathList.wsf" /bom /dbg /dir /c /d /f /t `> $tempFile
+Get-Content -LiteralPath $tempFile |
+	Set-Content -LiteralPath "$workDir\tools\$osVersion $cpu $edition $now.txt" -Encoding UTF8
+Remove-Item -LiteralPath $tempFile
 
 Push-Location $workDir\tools
 
