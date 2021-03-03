fb3324b4446e70f6288467a3c28dcee0e1a787e9
baldcat18 <baldcat@outlook.jp>
2021-03-03 20:53:15 +0900

    ファイルを上書きする方法を改善した

--- tools/Set-Index.ps1
+++ tools/Set-Index.ps1
@@ -10,15 +10,17 @@ param()
 
 Set-StrictMode -Version Latest
 
-$dataFile = (Resolve-Path "$PSScriptRoot\..\src\modules\data.js").Path
+$modules = (Resolve-Path "$PSScriptRoot\..\src\modules").Path
+$dataFile = "$modules\data.js"
+$tmpFile = "$modules\data.js.tmp"
 
-$tmpLine = New-Object System.Collections.Generic.List[string] 2048
 $index = 0
+
+$encoding = if ($PSVersionTable['PSVersion'] -ge '6.0') { 'utf8BOM' } else { 'UTF8' }
 
 Get-Content -LiteralPath $dataFile |
 	ForEach-Object {
-		$tmpLine.Add($(if ($_ -cmatch '^\t\t\tcase \d+:$') { "`t`t`tcase " + $index++ + ':' } else { $_ }))
-	}
-
-$encoding = if ($PSVersionTable['PSVersion'] -ge '6.0') { 'utf8BOM' } else { 'UTF8' }
-Set-Content -LiteralPath $dataFile ($tmpLine -join "`r`n") -Encoding $encoding
+		if ($_ -cmatch '^\t\t\tcase \d+:$') { "`t`t`tcase $(($index++)):" } else { $_ }
+	} |
+	Out-File -LiteralPath $tmpFile -Encoding $encoding
+[System.IO.File]::Replace($tmpFile, $dataFile, [nullstring]::Value)
